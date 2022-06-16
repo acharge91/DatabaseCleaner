80e3703b95cde1ce048c0fc42ce62c265aa5274c
Josh A <joshaustin312@gmail.com>
2022-06-16 15:54:06 +0100

    added class to handle deleting all records from database and matching query in interface

---
+++ src/main/java/com/sparta/ja/DBRecordsDeleter.java
@@ -0,0 +1,25 @@
+package com.sparta.ja;
+
+import com.sparta.ja.jdbc.ConnectionManager;
+import com.sparta.ja.jdbc.EmployeeDAO;
+import com.sparta.ja.jdbc.SQLQueries;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBRecordsDeleter {
+
+    private static final Connection DBConnection = ConnectionManager.getConnection();
+
+    public static void deleteAllRecordsFromDatabase(){
+        try {
+            PreparedStatement preparedStatement = DBConnection.prepareStatement(SQLQueries.DELETE_ALL_RECORDS);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
--- src/main/java/com/sparta/ja/jdbc/SQLQueries.java
+++ src/main/java/com/sparta/ja/jdbc/SQLQueries.java
@@ -6,5 +6,6 @@ public interface SQLQueries {
     String SELECT_ALL = "SELECT * FROM public.employees";
     String INSERT_INTO_DB = "INSERT INTO public.employees (\"empId\", \"namePrefix\", \"firstName\", \"middleInitial\", \"lastName\", \"gender\", " +
             "\"email\", \"dob\", \"dateOfJoining\", \"salary\") VALUES (?,?,?,?,?,?,?,?,?,?)";
+    String DELETE_ALL_RECORDS = "TRUNCATE employees;";
 }
 
